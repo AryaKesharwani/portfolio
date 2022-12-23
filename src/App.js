a80c2438ccf37787e44f50833a7c8cc383a3360f
Arya kesarwani <aryakesharwani1234@gmail.com>
2022-12-23 18:44:42 +0530

    init

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Header from "./components/header/Header";
+import "./App.css";
+import Home from "./components/home/Home";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <Home />
+      </main>
+    </>
+  );
+}
+
+export default App;
